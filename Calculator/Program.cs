827a042919c6a9e45fe6d51eb17f512fb5f0597a
Tiago Souza <tiago0214@gmail.com>
2024-01-07 12:28:10 -0300

    att

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
