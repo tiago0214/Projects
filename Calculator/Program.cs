36a0efe44d8a5794b1f39abeeabb6cd7e377a2f3
Tiago Souza <tiago0214@gmail.com>
2024-01-07 17:14:29 -0300

    att

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,11 +6,57 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Soma();
+        }
+        static void Soma()
+        {
             Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
+            Console.WriteLine("Primeiro valor:");
             float v1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+            Console.WriteLine($"A soma dos dois valores é: {resultado}");
+        }
+        static void Subtrair()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"A subtração dos dois valores é: {resultado}");
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"A multiplicação dos dois valores é: {resultado}");
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"A divisao dos dois valores é: {resultado}");
         }
     }
+
+
 }
