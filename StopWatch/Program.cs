fbced583fbe6ec3568a873cf206c6b14224ca7b8
Tiago Souza <tiago0214@gmail.com>
2024-01-08 15:23:04 -0300

    att

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -14,15 +14,48 @@ namespace StopWatch
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S - Segundos");
-            Console.WriteLine("M - Minutos");
+            Console.WriteLine("S - Segundos => 10s = 10 segundos");
+            Console.WriteLine("M - Minutos => 1m = 1 minuto");
             Console.WriteLine("0 - Sair");
             Console.WriteLine("Quanto tempo deseja contar ?");
 
             string data = Console.ReadLine().ToLower();
-            Console.WriteLine(data);
+            if (data == "0")
+            {
+                System.Environment.Exit(0);
+            }
+            if (data != "0")
+            {
+                char type = char.Parse(data.Substring(data.Length - 1, 1));
+                int time = int.Parse(data.Substring(0, data.Length - 1));
+                int multiplier = 1;
+
+                if (type == 'm')
+                    multiplier = 60;
+                PreStart(time * multiplier);
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido, digite novamente!");
+                Menu();
+            }
 
         }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Preparar...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Apontar...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Fogo...");
+            Thread.Sleep(1000);
+            Start(time);
+        }
+
 
         static void Start(int time)
         {
@@ -39,6 +72,7 @@ namespace StopWatch
             Console.Clear();
             Console.WriteLine("Stop watch finalizado.");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
