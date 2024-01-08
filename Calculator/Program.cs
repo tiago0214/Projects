96b8beb682a9fe280607d39b1d133412d16bbd1c
Tiago Souza <tiago0214@gmail.com>
2024-01-07 22:25:33 -0300

    att

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,9 +6,32 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Soma();
+            Menu();
         }
-        static void Soma()
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Seja bem-vindo!\nDigite uma opção no menu!");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("3 - Multiplicar");
+            Console.WriteLine("4 - Dividir");
+            int valor = int.Parse(Console.ReadLine());
+
+            switch (valor)
+            {
+                case 1: Somar(); break;
+                case 2: Subtrair(); break;
+                case 3: Multiplicacao(); break;
+                case 4: Divisao(); break;
+                default:
+                    Console.WriteLine("Opção invalida");
+                    break;
+            }
+
+
+        }
+        static void Somar()
         {
             Console.Clear();
             Console.WriteLine("Primeiro valor:");
@@ -19,6 +42,18 @@ namespace Calculator
 
             float resultado = v1 + v2;
             Console.WriteLine($"A soma dos dois valores é: {resultado}");
+
+            Console.WriteLine("");
+            Console.WriteLine("1 - para fazer outra operação");
+            Console.WriteLine("2 - para sair");
+            int valor = int.Parse(Console.ReadLine());
+
+            switch (valor)
+            {
+                case 1: Menu(); break;
+                case 2: Console.Clear(); break;
+                default: Console.WriteLine("Opção invalida, desligando o sistema."); break;
+            }
         }
         static void Subtrair()
         {
@@ -31,6 +66,19 @@ namespace Calculator
 
             float resultado = v1 - v2;
             Console.WriteLine($"A subtração dos dois valores é: {resultado}");
+
+            Console.WriteLine("");
+            Console.WriteLine("1 - para fazer outra operação");
+            Console.WriteLine("2 - para sair");
+            int valor = int.Parse(Console.ReadLine());
+
+            switch (valor)
+            {
+                case 1: Menu(); break;
+                case 2: Console.Clear(); break;
+                default: Console.WriteLine("Opção invalida, desligando o sistema."); break;
+            }
+
         }
         static void Multiplicacao()
         {
@@ -43,6 +91,19 @@ namespace Calculator
 
             float resultado = v1 * v2;
             Console.WriteLine($"A multiplicação dos dois valores é: {resultado}");
+
+            Console.WriteLine("");
+            Console.WriteLine("1 - para fazer outra operação");
+            Console.WriteLine("2 - para sair");
+            int valor = int.Parse(Console.ReadLine());
+
+            switch (valor)
+            {
+                case 1: Menu(); break;
+                case 2: Console.Clear(); break;
+                default: Console.WriteLine("Opção invalida, desligando o sistema."); break;
+            }
+
         }
         static void Divisao()
         {
@@ -55,8 +116,19 @@ namespace Calculator
 
             float resultado = v1 / v2;
             Console.WriteLine($"A divisao dos dois valores é: {resultado}");
+
+            Console.WriteLine("");
+            Console.WriteLine("1 - para fazer outra operação");
+            Console.WriteLine("2 - para sair");
+            int valor = int.Parse(Console.ReadLine());
+
+            switch (valor)
+            {
+                case 1: Menu(); break;
+                case 2: Console.Clear(); break;
+                default: Console.WriteLine("Opção invalida, desligando o sistema."); break;
+            }
+
         }
     }
-
-
 }
